82afb204b66db15af221b99268511fe728c912bd
Igor Berlenko <igorberlenko7@gmail.com>
2023-11-24 23:04:13 +0800

    up

---
+++ assets/canisterStatus-N5J2XGIM-aa87cdf9.js
@@ -0,0 +1,1 @@
+import{Y as o,e as r}from"./index-11150de3.js";export{o as encodePath,r as request};
--- assets/canisterStatus-N5J2XGIM-f044c0af.js
+++
@@ -1,1 +0,0 @@
-import{Y as o,e as r}from"./index-a0b0f817.js";export{o as encodePath,r as request};
